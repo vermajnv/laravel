a90da0458344b33993300eaca203e2d5501e2110
vermajnv <nayanrahul.jnv@gmail.com>
2018-09-19 16:00:49 +0530

    This is the first commit to laravel project from scrach

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+
+// Route::get('/hello', function () {
+//     return "<h1>Hello World</h1>";
+// });
+// Route::get('/hello/print', function () {
+//     return "Hello World";
+// });
+// create dynamic routing as users
+// Route::get('/users/{id}/{name}', function($id, $name) {
+//     return "<h2> This is users . $id . and name is .$name</h2>";
+// });
+
+Route::get('/', 'PagesController@index');
+
+// adding a page route
+Route::get('/about', 'PagesController@about');
+
+// adding Services page using controller
+
+Route::get('/services', 'PagesController@services');
+
